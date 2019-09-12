6537d381c635a91f2a81877e436a5adb32fa2638
wangzheng <wangzheng@mail.china.cn>
2019-09-12 16:06:59 +0800

    custom config

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,15 @@
 const { override, fixBabelImports } = require('customize-cra');
-module.exports = override(fixBabelImports('import', {
-    libraryName: 'antd-mobile',
-    style: 'css'
-}))
+const rewrite = (regExp, opts) => config => {
+    if (config.mode === 'production') {
+        const loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf));
+        const loader = loaders.oneOf.find(cfg => cfg.loader.match(regExp));
+        Object.keys(opts).forEach(key => {
+            loader.options[key] = opts[key];
+        });
+    }
+    return config;
+}
+module.exports = override(
+    fixBabelImports('import', { libraryName: 'antd-mobile', style: 'css' }),
+    rewrite(/url-loader/, { limit: 5 * 1024 })
+)
--- src/views/home.js
+++ src/views/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link,NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Carousel} from 'antd-mobile';
 import FooterBar from './common/footerbar'
 import '../css/home.css';
